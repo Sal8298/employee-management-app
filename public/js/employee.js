ed400549c8cd8cdede0dc0b897f97e8fb35b5d46
Sal8298 <salahuddinasim95@gmail.com>
2023-04-02 13:28:41 -0500

    Added required packages. made an employee.js file.

---
+++ public/js/employee.js
@@ -0,0 +1,12 @@
+//Dependancies
+const consoleTable = require("console.table");
+const mysql = require("mysql2"); 
+const inquirer = require("inquirer");
+
+
+function employeeMenu () {
+
+}
+
+
+module.exports = employeeMenu;
---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mysql = require('mysql2');
+const employeeMenu = require('./public/js/employee');
+
+
+employeeMenu();
+
+
+
